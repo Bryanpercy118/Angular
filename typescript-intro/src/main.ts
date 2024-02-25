bfb114742aef7d4671e28522f62a90b1ef7ac08e
bryanpercy118 <bryanpercy77@gmail.com>
2024-02-25 13:11:58 -0500

    Object Interfaces

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -1,9 +1,9 @@
 import './style.css'
-import './topics/01-basic-types.ts'
+//import './topics/01-basic-types.ts'
+import './topics/02-object-interface.ts'
+
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  Hola mundo
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ typescript-intro/src/topics/02-object-interface.ts
@@ -0,0 +1,23 @@
+
+
+const skills: string[] = ['Bash', 'Counter', 'Healing'];
+
+interface Character {
+    name:string,
+    hp:number,
+    skills:string[],
+    hometown?:string;
+}
+
+
+const strider : Character = {
+    name:'Strider',
+    hp:100,
+    skills:['Bash','Counter'],
+}
+
+strider.hometown='New York';
+
+console.table(strider);
+
+export { }
