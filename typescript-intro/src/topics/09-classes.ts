6cc142818421e0d4004586a881fdb30bc2c5f472
bryanpercy118 <bryanpercy77@gmail.com>
2024-02-26 11:36:14 -0500

    Classes Refactoring

--- typescript-intro/src/topics/09-classes.ts
+++ typescript-intro/src/topics/09-classes.ts
@@ -1,15 +1,33 @@
-export class Person{
-    public name?: string;
-    public age?: number;
-    private address?: string;
-
-    constructor() {
-        this.name = 'John';
-        this.age = 20;
-        this.address = 'New York';
-    }
+export class Person {
+    constructor(
+        public firstname: string, 
+        public lastname: string,
+        private address: string ='No Address'
+        ) {}
 }
 
+// export class Hero extends Person{
 
-const iroman = new Person();
-console.log(iroman.name);
+//     constructor(
+//         public alterEgo:string,
+//         public age:number,
+//         public realName:string
+//     ){
+//      super(realName,'New York');
+//     }
+// }
+
+export class Hero {
+    //public person:Person;
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ) {}
+}
+
+   
+const tony = new Person('Tony', 'Stark', 'New York');
+const iroman = new Hero('Iroman', 45, 'Tony',tony);
+console.log(iroman);
